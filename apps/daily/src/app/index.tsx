a25312a773e7f4231e4b731a61cb2c1010197deb
Su Anli <su.anli@anlitz.com>
2024-09-12 16:12:20 +0800

    feat: add react-native-reusables

--- apps/daily/src/app/index.tsx
+++ apps/daily/src/app/index.tsx
@@ -1,7 +1,12 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+
+import { Button, Text } from '@shared/ui';
 
 export default () => (
   <View className="flex-1 items-center justify-center">
+    <Button>
+      <Text>Test</Text>
+    </Button>
     <Text className="text-red-500">Home page</Text>
   </View>
 );
---
+++ libs/shared/ui/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/constants';
+export * from './lib/useColorScheme';
+export * from './ui/button';
+export * from './ui/text';
